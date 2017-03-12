54ac1db56f3eb94508c9e82d4d1c0ce3958bd969
Jesus Rosas <jesus@ecotechnologic.com.mx>
2017-03-11 20:36:43 -0600

    static files in expressjs

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var app = express(); 
 
+
+app.set('view engine', 'pug');
+
+app.use(express.static(__dirname + '/public'));
+
 app.get("/", function(req, res) {
-	res.send("Hola Mundo!");
+	res.render('index')
 });
+
 
 app.listen(3000, function(err) {
 	if (err) return console.log("Hubo un error"), process.exit(1);
