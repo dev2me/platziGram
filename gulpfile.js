8a60e8b1b76f75fecf474026b1433272ef45361f
Jesus Rosas <jesus@ecotechnologic.com.mx>
2017-03-12 00:42:14 -0600

    se añade sourc-stream

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,8 @@ var rename = require('gulp-rename');
 var babel = require('babelify');
 var browserify = require('browserify');
 var preset =  require('babel-preset-es2015');
+var source = require('vinyl-source-stream');
+
 
 gulp.task('styles', function() {
   gulp
@@ -22,7 +24,7 @@ gulp.task('scripts', function() {
   browerify('./src/index.js')
   .transform(babel, preset)
   .bundle()
-  .pipe(souce('index.js'))
+  .pipe(source('index.js'))
 })
 
 gulp.task('default', ['assets','styles', 'scripts']);
